94e4d3b6180216fe692fa1d849a154e2c9022088
zhudz <zdzserver@163.com>
2023-11-16 18:57:42 +0800

    调整ReadLineFunc方法

--- file.go
+++ file.go
@@ -1,24 +1,17 @@
 package goutils
 
 import (
-	"bufio"
 	"os"
 )
 
-// ReadLineFunc read the file line by line and call f(c) to process each line of data
-func ReadLineFunc(path string, f func(line string) error) error {
+// ReadFileLineFunc read the file line by line and call f(c) to process each line of data
+func ReadFileLineFunc(path string, f func(num int, line string) bool) error {
 	file, err := os.Open(path)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		if err = f(BytesToString(scanner.Bytes())); err != nil {
-			return err
-		}
-	}
-	return err
+	return ReadLineFunc(file, f)
 }
 
 // IsDir reports whether this path is a directory
