a96825db4c6a330ccc166cc804e684b3b8219768
zhudz <zdzserver@163.com>
2023-11-22 18:21:33 +0800

    删除不成熟的方法

--- ioutils/scan_test.go
+++ ioutils/scan_test.go
@@ -1,9 +1,7 @@
 package ioutils
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strings"
 	"testing"
 )
@@ -24,77 +22,4 @@ func TestReadLineFunc(t *testing.T) {
 		return true
 	})
 	t.Logf("err: %v", err)
-}
-
-func TestForwardScanner_Scan(t *testing.T) {
-	//	str := `
-	//789
-	//456
-	//123`
-	//	reader := strings.NewReader(str)
-	//size := int64(len(str))
-	reader, err := os.Open("/var/log/system.log")
-	if err != nil {
-		t.Fatal(err)
-	}
-	fileInfo, err := reader.Stat()
-	if err != nil {
-		t.Fatal(err)
-	}
-	size := fileInfo.Size()
-	scanner := NewForwardScanner(reader, size)
-
-	for scanner.Scan() {
-		fmt.Println("---", fmt.Sprintf("%q", scanner.Text()))
-	}
-
-	t.Logf("err: %v", scanner.Err())
-}
-
-func TestScanner_Scan(t *testing.T) {
-	str := `
-789
-456
-123
-`
-	reader := strings.NewReader(str)
-	scanner := bufio.NewScanner(reader)
-
-	for scanner.Scan() {
-		fmt.Println("---", fmt.Sprintf("%q", scanner.Text()))
-	}
-
-	t.Logf("err: %v", scanner.Err())
-}
-
-func BenchmarkScanner_Scan(b *testing.B) {
-	file, err := os.Open("/var/log/system.log")
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			scanner.Text()
-		}
-	}
-
-}
-
-func BenchmarkForwardScanner_Scan(b *testing.B) {
-	file, err := os.Open("/var/log/system.log")
-	if err != nil {
-		b.Fatal(err)
-	}
-	fileInfo, err := file.Stat()
-	if err != nil {
-		b.Fatal(err)
-	}
-	for i := 0; i < b.N; i++ {
-		scanner := NewForwardScanner(file, fileInfo.Size())
-		for scanner.Scan() {
-			scanner.Text()
-		}
-	}
 }
